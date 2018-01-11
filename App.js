6cf734ce561ef25f259b5120460641f136e7e38e
pranav.dixit <pranav.dixit@equinox.com>
2018-01-11 15:17:19 +0530

    text change

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends Component<{}> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native! eurekaa!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
